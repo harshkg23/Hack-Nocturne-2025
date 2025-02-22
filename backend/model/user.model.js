171490401883ae55c587ee668e518d73a21922fc
HIMANSHU. <himanshu3141@gmail.com>
2025-02-23 04:15:19 +0530

    Refactor token generation method in user model to simplify access token creation

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -45,27 +45,13 @@ userSchema.methods.isPasswordCorrect=async function(password){
   return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken = function(){
+export const createToken = (id) => {
     return jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullName: this.fullName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
+      { id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" } 
+    );
+  };
 
 export const User=mongoose.model("User",userSchema)
 
-export const createToken = (id) => {
-  return jwt.sign(
-    { id },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" } // Optionally, set an expiration time for the token
-  );
-};
