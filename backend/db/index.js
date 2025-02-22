c0273e8f08cdc1c42dc9dcf2f8e0842904a3d6e1
HIMANSHU. <himanshu3141@gmail.com>
2025-02-23 05:16:10 +0530

    Refactor MongoDB connection logging and error handling in database index

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
@@ -6,13 +6,12 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    // console.log(connectionInstance);
 
     console.log(
-      `\n MongoDB connected !! DB host:${connectionInstance.connection.host}`
+      `\n MongoDB connected! DB host: ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("MONGODB connection failed", error);
+    console.error("MONGODB connection failed", error);
     process.exit(1);
   }
 };
