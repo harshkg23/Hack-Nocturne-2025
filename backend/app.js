4a7a608ad6dfe6f0f1e44c8b2a9848e7f3107267
Harsh Kumar Gupta <harshkumargupta2023@gmail.com>
2025-02-23 04:22:17 +0530

    Add user routes to the main application file

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./routes/user.routes.js";
 
 const app = express();
 
@@ -16,5 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+app.use("/users", router);
 
 export { app };
