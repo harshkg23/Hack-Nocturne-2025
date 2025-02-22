448fad3fc6a87a63e8231fd98b958a8d919a24ba
Harsh Kumar Gupta <harshkumargupta2023@gmail.com>
2025-02-22 18:33:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-100'>hello</h1>
+    </>
+  )
+}
+
+export default App
