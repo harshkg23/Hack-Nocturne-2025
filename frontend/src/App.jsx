7e5cd44e01bed85065e04064a2d6d6402f9a04a7
Harsh Kumar Gupta <harshkumargupta2023@gmail.com>
2025-03-28 15:51:49 +0530

    add main application structure with routing, layout, and styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout/Layout";
+import Landing from "./pages/Landing/Landing";
+// import Signup from "./pages/Signup";
+// import UserLogin from "./pages/Login";
+// import Dashboard from "./pages/dashboard/Dashboard";
+// import TechnicalNews from "./pages/technical-news/TechnicalNews";
+// import NewsDetail from "./pages/NewsDetails/NewsDetails";
+// import HotTopics from "./pages/hot-topic/HotTopics";
+// import TopicDetail from "./pages/hot-topic/Topicdetail";
+// import SoftwareTrends from "./pages/softwareTrends/SoftwareTrends";
+// import Logout from "./pages/Logout";
+// import ProtectedRoute from "../ProtectedRoute.jsx";
+
+
+function App() {
+  const url = "http://localhost:8000";
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Landing />} />
+        {/* <Route path="login" element={<UserLogin url={url} />} />
+        <Route path="signup" element={<Signup url={url} />} /> */}
+
+        {/* Protected Routes */}
+        {/* <Route element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="news" element={<TechnicalNews />} />
+          <Route path="news/:id" element={<NewsDetail />} />
+          <Route path="hot-topics" element={<HotTopics />} />
+          <Route path="topics/:id" element={<TopicDetail />} />
+          <Route path="software-trends" element={<SoftwareTrends />} />
+          <Route path="logout" element={<Logout />} />
+        </Route> */}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
